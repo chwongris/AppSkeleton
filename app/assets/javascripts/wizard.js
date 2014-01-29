f1a8112a375de46061d75ede7980820019c726fd
Chris Wong <chwongris@gmail.com>
2014-01-29 17:45:03 -0500

    wizard page

---
+++ app/assets/javascripts/wizard.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+$(".question").on( "keypress", function(event){
+  if(event.which == 13) { 
+    var question = $(".question").val();    
+    $("#slide-1").hide();    
+    $(".question").hide();
+    $(".matrix").show();
+    $("#matrix-title").html(question);
+    $(".options").show();
+    $("#slide-2").show();
+  }
+});
+
+var x = 4;
+$(".plus").on("click", function(){
+  $("#option-"+x).show();
+  x++;
+  if (x == 6){
+    $(this).hide();
+  }
+});
+
+});
